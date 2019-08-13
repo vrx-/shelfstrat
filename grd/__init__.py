b8b9f636cabaed179e847b5e568f9119eb1bf85b
vrx- <vrx@tamu.edu>
2019-08-13 11:18:22 -0500

    modified make_grd.py

--- grd/__init__.py
+++ grd/__init__.py
@@ -1,10 +1,11 @@
-#!/usr/bin/env python2.7
+#!/usr/bin/env python3.6
 # encoding: utf-8
 """
 __init__.py
 
 Created by Rob Hetland on 2013-05-27
-Copyright (c) 2013 Texas A&M Univsersity. All rights reserved.
+Modifiedn by Veronica Ruiz Xomchuk on 2019-08-12
+Copyright (c) 2019 Texas A&M Univsersity. All rights reserved.
 Release under MIT license.
 """
 
--- grd/make_grd.py
+++ grd/make_grd.py
@@ -1,75 +1,69 @@
-""" This script creates a grid and forcing file for an idealized near-field plume case
+""" This script creates a grid file for an idealized near-field plume case to evaluate bottom traping by a bump
 """
-
-
-
 import os
-import cPickle
-
 import numpy as np
-# import matplotlib.pyplot as plt
-import netCDF4
-
-import octant
-import octant.roms
+import xarray as xr
 
 
+def make_CGrid(x, y):
+    ds = xr.Dataset({'x_vert': (['eta_vert', 'xi_vert'], x),
+                     'y_vert': (['eta_vert', 'xi_vert'], y)})
+    ds['x_rho'] = (['eta_rho', 'xi_rho'], 0.25 * (x[1:, 1:] + x[1:, :-1] + x[:-1, 1:] + x[:-1, :-1]))
+    ds['y_rho'] = (['eta_rho', 'xi_rho'], 0.25 * (y[1:, 1:] + y[1:, :-1] + y[:-1, 1:] + y[:-1, :-1]))
+    ds['x_psi'] = (['eta_psi', 'xi_psi'], x[1:-1, 1:-1])
+    ds['y_psi'] = (['eta_psi', 'xi_psi'], y[1:-1, 1:-1])
+    ds['x_u'] = (['eta_u', 'xi_u'], 0.5 * (x[:-1, 1:-1] + x[1:, 1:-1]))
+    ds['y_u'] = (['eta_u', 'xi_u'], 0.5 * (y[:-1, 1:-1] + y[1:, 1:-1]))
+    ds['x_v'] = (['eta_v', 'xi_v'], 0.5 * (x[1:-1, :-1] + x[1:-1, 1:]))
+    ds['y_v'] = (['eta_v', 'xi_v'], 0.5 * (y[1:-1, :-1] + y[1:-1, 1:]))
+    ds['x_psi'] = (['eta_psi', 'xi_psi'], x[1:-1, 1:-1])
+    ds['y_psi'] = (['eta_psi', 'xi_psi'], y[1:-1, 1:-1])
 
-def make_grd(rootdir = '../project/',
-             Hmin = 5.0, alpha = 0.001, 
-             f = 1e-4,
-             dx = 1e3, dy = 1e3,
-             shp = (131, 259)):
-    
+    x_temp = 0.5 * (ds.x_vert[1:, :] + ds.x_vert[:-1, :])
+    y_temp = 0.5 * (ds.y_vert[1:, :] + ds.y_vert[:-1, :])
+    dx = np.sqrt(np.diff(x_temp, axis=1)**2 + np.diff(y_temp, axis=1)**2)
+    x_temp = 0.5 * (ds.x_vert[:, 1:] + ds.x_vert[:, :-1])
+    y_temp = 0.5 * (ds.y_vert[:, 1:] + ds.y_vert[:, :-1])
+    dy = np.sqrt(np.diff(x_temp, axis=0)**2 + np.diff(y_temp, axis=0)**2)
+
+    ds['pm'] = (['eta_rho', 'xi_rho'], 1. / dx)
+    ds['pn'] = (['eta_rho', 'xi_rho'], 1. / dy)
+    return ds
+
+
+def make_grd(rootdir='../project/',
+             Hmin=5.0, alpha=0.001,
+             ho=20., dh=0., wdh=1e4,
+             f=1e-4,
+             dx=1e3, dy=1e3,
+             shp=(131, 259)):
+
     if not os.path.exists(rootdir):
-        print ' ### Making directory %s' % rootdir
+        print('Making directory %s' % rootdir)
         os.mkdir(rootdir)
-    
-    M, L = shp
-    x = np.arange(L, dtype='d') * dx
-    y = np.arange(M, dtype='d') * dy
-    
+
+    x = np.arange(shp[1], dtype='d') * dx
+    y = np.arange(shp[0], dtype='d') * dy
+
     xvert, yvert = np.meshgrid(x, y)
-    
-    grd = octant.grid.CGrid(xvert, yvert)
-    
+
+    grd = make_CGrid(xvert, yvert)
+
     # custom set coreolis paremeter (otherwise determined from grd.lat)
-    grd.f = f
-    
-    # create an exponential depth profile, with decay radius Rh, and a value
-    # of Hmin and Hmax at the river and ocean ends, respectively.
-    cff1 = (grd.y_rho - grd.y_rho[1])*alpha + Hmin
+    grd['f'] = f
+
+    # create a depth profile with slope alpha, a value of Hmin
+    # and a bump localized approximately at a depth ho, as a function of step height (dh) and width (wdh).
+    cff1 = alpha * grd.y_rho + dh * np.tanh((grd.y_rho - (ho - Hmin) / alpha) / wdh * 4) + Hmin
     cff1 += 0.01 * np.random.randn(*grd.y_rho.shape) * cff1
-    cff1[0] = cff1[1]
-    cff2 = Hmin
-    grd.h = np.maximum(cff1, cff2)
-    
-    # plt.ioff()
-    # fig = plt.figure()
-    # ax = fig.add_subplot(111)
-    # ax.plot(grd.x_psi/1000.0, grd.y_psi/1000.0, '-k', linewidth=0.1)
-    # ax.plot(grd.x_psi.T/1000.0, grd.y_psi.T/1000.0, '-k', linewidth=0.1)
-    # hm = np.ma.masked_where(grd.mask==0, grd.h)
-    # pc = ax.pcolormesh(xvert/1000.0, yvert/1000.0, hm)
-    # plt.colorbar(pc, orientation='horizontal')
-    # ax.set_xlim(0, xvert.max()/1000.0)
-    # ax.set_ylim(0, yvert.max()/1000.0)
-    # ax.set_aspect(1.0)
-    # plt.savefig(rootdir + '/grd.png', dpi=300)
-    # plt.close(fig)
-    
-    print 'pickling grid..'
-    f = open(os.path.join(rootdir, 'grd.pickle'), 'w')
-    cPickle.dump(grd, f)
-    f.close()
-    
-    grd.proj = None  # define non-georeference grid
-    print 'Writing netcdf GRD file..'
-    octant.roms.write_grd(grd, os.path.join(rootdir, 'shelfstrat_grd.nc'), verbose=True)
+    grd['h'] = np.maximum(cff1, Hmin)
+
+    print('Writing netcdf GRD file..')
+    grd.to_netcdf(os.path.join(rootdir, 'shelfstrat_grd.nc'))
+
 
 if __name__ == '__main__':
     from os.path import expanduser
     home = expanduser("~")
-    
-    # make_grd(rootdir='../project/test_grid')
-    make_grd(rootdir=os.path.join(home, 'Projects/shelfstrat/project/test'), shp=(131, 259))
+
+    make_grd(rootdir=os.path.join(home, 'Projects/shelfstrat/tests/'))
