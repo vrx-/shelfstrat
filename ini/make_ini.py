4b6d78f4c4629e20f646e9725e50a949f2ca56d3
vrx- <vrx@tamu.edu>
2019-08-22 20:00:46 -0500

    i broke the ini file

--- ini/make_ini.py
+++ ini/make_ini.py
@@ -16,13 +16,12 @@ def C(theta_s, theta_b, s):
         return -C
 
 
-def make_ini(output='../tests/shelfstrat_ini.nc',
-             grd_path='../tests/shelfstrat_grd.nc',
+def make_ini(grd_path='../tests/shelfstrat_grd.nc', output='../test/shelfstrat_ini.nc',
              zlevs=30, theta_s=3.0, theta_b=0.4, hc=5.0,
              R0=1027.0, T0=25.0, S0=35.0, TCOEF=1.7e-4, SCOEF=7.6e-4,
              M20=1e-7, M2_yo=50e3, M2_r=5e3,
              N20=1e-4, N2_zo=50.0, N2_r=50.0,
-             balanced_run: True):
+             balanced_run=True):
     '''
     Create an initialization file.
 
@@ -38,6 +37,7 @@ def make_ini(output='../tests/shelfstrat_ini.nc',
     TCOEF == 1.7d-4                     ! 1/Celsius
     SCOEF == 7.6d-4                     ! 1/PSU
     '''
+
     grd = xr.open_dataset(grd_path)
     g = 9.81
     dy = 1 / grd.pn
@@ -65,20 +65,24 @@ def make_ini(output='../tests/shelfstrat_ini.nc',
     for n in range(zlevs):
         temp[n] = T0 - np.trapz(N2[n:] / (g * TCOEF), x=z[n:], axis=0)
 
+    #########################################
     # Create dataset
 
     ds = xr.Dataset({'temp': temp, 'salt': salt,
                      's_rho': s_rho, 'xi_rho': grd.xi_rho, 'eta_rho': grd.eta_rho})
 
     if balanced_run:
-        rhs = M2 * Hz / grd.f
+        rhs = Hz * M2 / grd.f
         u_z = 0.5 * (rhs[:, :, 1:] + rhs[:, :, :-1])
         u = np.cumsum(u_z, axis=0)
         ubottom = np.zeros((1, u.shape[1], u.shape[2]))
         u = np.concatenate((ubottom, u))
         u = 0.5 * (u[1:] + u[:-1])
     else:
-        u = 0
+        u = 0.
+
+    ds['ocean_time'] = xr.DataArray([0.0], dims=['ocean_time'])
+    ds['ocean_time'].attrs['units'] = 'days'
 
     ds['u'] = xr.DataArray(u[np.newaxis, :, :, :],
                            dims=['ocean_time', 's_rho', 'eta_u', 'xi_u'],
@@ -95,12 +99,11 @@ def make_ini(output='../tests/shelfstrat_ini.nc',
     ds['vbar'] = xr.DataArray(np.zeros((1, grd.dims['eta_v'], grd.dims['xi_v'])),
                               dims=['ocean_time', 'eta_v', 'xi_v'],
                               attrs={'units': 'm s-1'})
+
+    ds.attrs['type'] = 'ROMS FRC file'
     ds.attrs['Description'] = 'Initial conditions for ideal shelf'
     ds.attrs['Author'] = 'Vero and Lixin'
     ds.attrs['Created'] = datetime.now().isoformat()
-    ds.attrs['type'] = 'ROMS FRC file'
-    ds['ocean_time'] = xr.DataArray([0.0], dims=['ocean_time'])
-    ds['ocean_time'].attrs['units'] = 'days'
     print('Writing netcdf INI file..')
     ds.to_netcdf(output)
 
